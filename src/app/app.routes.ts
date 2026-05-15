7e09c21a992fd91555581d2cca81707fa1ed6f3d
Leandro <leandro.guia.dev@gmail.com>
2026-05-15 18:03:02 -0300

    Agrego favIcon y agrego rutas para los nuevos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent: () => import('./pages/home-page/home-page').then(m=>m.HomePage)
+    },
+    {
+        path:'about',
+        loadComponent: () => import('./pages/about-page/about-page').then(m=>m.AboutPage)
+    }
+];
