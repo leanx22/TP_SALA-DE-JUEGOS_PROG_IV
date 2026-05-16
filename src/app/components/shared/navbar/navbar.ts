76fa9ef25a71070f2d833deab9b2310a1bca70b1
Leandro <leandro.guia.dev@gmail.com>
2026-05-16 20:03:00 -0300

    Actualización de la navbar para cambiar su información dependiendo del estado de la sesión

--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { NgbCollapse, NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { SupaAuthService } from '../../../services/supabase/supa-auth-service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,28 +12,38 @@ import { NgbCollapse, NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap
     RouterLink,
     RouterLinkActive,
     CommonModule,
-    NgbDropdownModule
+    NgbDropdownModule,
   ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-  private router = inject(Router);
+  private readonly supaAuth = inject(SupaAuthService);
+  private readonly router = inject(Router);
 
   isMenuOpen = false;
   isLogoCollapsed = true;
   absoluteRoutes = [''];
   hideRoutes = ['/session/login', '/session/sign-up'];
 
-  toggleLogoCollapse(){
+  toggleLogoCollapse() {
     this.isLogoCollapsed = !this.isLogoCollapsed;
   }
 
-  isAbsolute():boolean{
+  getCurrentUser() {
+    return this.supaAuth.currentUser();
+  }
+
+  isAbsolute(): boolean {
     return this.absoluteRoutes.includes(this.router.url);
   }
 
-  shouldHide():boolean{
+  shouldHide(): boolean {
     return this.hideRoutes.includes(this.router.url);
+  }
+
+  async logOut() {
+    await this.supaAuth.logOut();
+    this.router.navigate(['/session/login']);
   }
 }
